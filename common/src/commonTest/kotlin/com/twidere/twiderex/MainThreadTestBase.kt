ec3026379e935f0b6e3c794e3384caf66b11cc38
seiko <605590140@qq.com>
2021-12-21 13:46:17 +0800

    update MainThreadTestBase

--- common/src/commonTest/kotlin/com/twidere/twiderex/MainThreadTestBase.kt
+++ common/src/commonTest/kotlin/com/twidere/twiderex/MainThreadTestBase.kt
@@ -20,28 +20,24 @@
  */
 package com.twidere.twiderex
 
-// import kotlinx.coroutines.DelicateCoroutinesApi
-// import kotlinx.coroutines.Dispatchers
-// import kotlinx.coroutines.ExperimentalCoroutinesApi
-// import kotlinx.coroutines.newSingleThreadContext
-// import kotlinx.coroutines.test.resetMain
-// import kotlinx.coroutines.test.setMain
-// import kotlin.test.AfterTest
-// import kotlin.test.BeforeTest
-//
-// @OptIn(ExperimentalCoroutinesApi::class)
-// internal open class MainThreadTestBase {
-//     @OptIn(DelicateCoroutinesApi::class)
-//     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-//
-//     @BeforeTest
-//     open fun setUp() {
-//         Dispatchers.setMain(mainThreadSurrogate)
-//     }
-//
-//     @AfterTest
-//     open fun tearDown() {
-//         Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
-//         mainThreadSurrogate.close()
-//     }
-// }
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal open class MainThreadTestBase {
+
+    @BeforeTest
+    open fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @AfterTest
+    open fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+    }
+}
